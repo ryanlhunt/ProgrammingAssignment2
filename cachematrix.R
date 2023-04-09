c1b32e2668653eb1cec5a6455ec8914f17065671
Ryan Hunt <ryanlhunt@outlook.com>
2023-04-09 17:07:55 -0500

    Update cachematrix.R
    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Inverse of a Matrix
+## Lexical Scoping
 
-## Write a short comment describing this function
-
+## Creates a matrix that can cache it's inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    # init
+    m <- NULL
+    
+    # Set matrix
+    set <- function(y) {
+          x <<- y
+          m <<- NULL
+    }
+    
+    # Get matrix
+    get <- function() x
+    
+    # Set inverse
+    set.inverse <- function(setinv) m <<- setinv
+    
+    # Get inverse
+    get.inverse <- function() m
+    
+    list(set = set, get = get,
+         set.inverse = set.inverse,
+         get.inverse = get.inverse)
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(wallet, ...) {
+    #gets the stored inverse
+    inv <- wallet$get.inverse()
+    # If it's there it returns it
+    if(!is.null(m)) {
+          message("looking for cache in wallet")
+          return(inv)
+    }
+    # It not stored, it calculates
+    data <- wallet$get()
+    m <- solve(data, ...)
+    wallet$set.inverse(m)
+    
+    
         ## Return a matrix that is the inverse of 'x'
+    m
 }
+
